d58018e8da3e06599bbe8d7a7e4a0f732ca17488
sdziobak <sarahdziobak@gmail.com>
2024-10-27 21:58:02 -0400

    I changed this to create table if not exists

--- database_connection.py
+++ database_connection.py
@@ -2,49 +2,43 @@ import sqlite3
 from user import User
 
 class DatabaseConnection:
-
-
     DATABASE_NAME = "password-hashing.db"
 
-
-    def add_user(user: User):
+    @staticmethod
+    def add_user(user):
         con = sqlite3.connect(DatabaseConnection.DATABASE_NAME)
         cur = con.cursor()
         cur.execute(f"INSERT INTO user VALUES ('{user.username}', '{user.hashing_algorithm}', '{user.hashed_password}', '{user.salt}')")
-        con.commit() 
+        con.commit()
         con.close()
 
-
-    def find_user(user : User):
-
+    @staticmethod
+    def find_user(user):
         con = sqlite3.connect(DatabaseConnection.DATABASE_NAME)
         cur = con.cursor()
         res = cur.execute(f"SELECT * FROM user WHERE username='{user.username}'")
-
         user_row = res.fetchone()
-
-        con.commit() 
+        con.commit()
         con.close()
-
-        if user_row == None:
+        if user_row is None:
             return None
-        else:  
+        else:
             return User(user_row[0], None, user_row[1], user_row[2], user_row[3])
-        
-
-        
-
-
-    # This method is only used for initial setup
+# had to update this so IF NOT EXISTS works
+    @staticmethod
     def create_user_table():
         con = sqlite3.connect(DatabaseConnection.DATABASE_NAME)
         cur = con.cursor()
-        cur.execute("CREATE TABLE user(username, hashing_algorithm, hashed_password, salt)")
-        con.commit() 
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS user (
+                username TEXT NOT NULL,
+                hashing_algorithm TEXT,
+                hashed_password TEXT,
+                salt TEXT
+            )
+        """)
+        con.commit()
         con.close()
-
-
-
 
 def tests():
     print("Test starting...")
@@ -52,7 +46,6 @@ def tests():
     DatabaseConnection.add_user(test_user)
     # print(DatabaseConnection.find_user(test_user))
     print("Test done.")
-
 
 if __name__ == "__main__":
     tests()
