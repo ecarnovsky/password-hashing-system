51eb28060e07801964275dcc00d14dc6b48b4922
sdziobak <sarahdziobak@gmail.com>
2024-11-02 15:55:50 -0400

    updated test to account for salting!

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,6 +20,7 @@ def test_get_algorithm_user_choice(monkeypatch, user_input, expected):
 
 @pytest.mark.parametrize("algorithm", list(HashingAlgorithm))
 def test_get_hashed_password(algorithm):
-    result = main.get_hashed_password("password123", algorithm)
-    assert isinstance(result, str)
-    assert len(result) > 10
+    hashed_password, salt = main.get_hashed_password("password123", algorithm)
+    assert isinstance(hashed_password, str)
+    assert len(hashed_password) > 10
+    assert salt is None or isinstance(salt, str)
